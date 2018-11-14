eeea1e4cf8dfa7e9d5ecb699c51ec7f7f8ce0e31
Colin Ronayne <cronayne@gmail.com>
2018-11-14 16:34:59 +0100

    Adding a second Trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +13,41 @@ import { trigger, state, style } from '@angular/animations';
       state('highlighted', style ({
         'background-color': 'blue',
         transform: 'translate(100px)'
-      }))
+      })),
+      transition('normal <=> highlighted', animate(300))
+      // transition('highlighted => normal', animate(800)),
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style ({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style ({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
+  wildState = 'normal';
   state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 
   onAdd(item) {
